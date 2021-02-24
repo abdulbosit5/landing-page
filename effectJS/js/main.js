60938ac947aa549320ce5c337ba96c497b5a41b6
Salom9796 <abdulbosit.e9697@gmail.com>
2021-02-25 01:12:04 +0500

    first commit

---
+++ effectJS/js/main.js
@@ -0,0 +1,22 @@
+
+function goout1(){
+    document.getElementById('ani1').style.display='block';
+    document.getElementById('ani2').style.display='none';
+    document.getElementById('ani3').style.display='none';
+    document.getElementById('bg').style.backgroundColor='#007C3F';
+
+}
+function goout2(){
+    document.getElementById('ani1').style.display='none';
+    document.getElementById('ani2').style.display='block';
+    document.getElementById('ani3').style.display='none';
+    document.getElementById('bg').style.backgroundColor='#DF6493';
+}
+function goout3(){
+    document.getElementById('ani1').style.display='none';
+    document.getElementById('ani2').style.display='none';
+    document.getElementById('ani3').style.display='block ';
+    document.getElementById('bg').style.backgroundColor='#CA3CAC';
+
+}
+
